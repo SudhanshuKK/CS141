f389c1eb1540f43cf5c6aff75645f8536fde0828
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-08-22 06:51:03 +0000

    Add files via upload

---
+++ lab4_q6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+float base, height, area;
+cout << "Given base and height of a triangle and we have to find its area \n";
+cout << "enter base and height\n";
+cin >> base >> height;
+area = 0.5*base*height;
+cout << " The area of triangle is - "<< area << endl;
+return 0;
+} 
