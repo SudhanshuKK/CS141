7a2eeabbb74c811fb2a166ee4f9666c4cf0d6ddb
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-08-30 15:25:30 +0000

    Add files via upload

---
+++ lab5_23.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=100;
+	cout << "we have to print the natural number between n to 1 \n";
+	//here n is 100;
+	while(a>=1) {
+	cout << a << endl;
+	a--;
+}
+return 0;
+}
+
+	
