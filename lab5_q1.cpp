139bc690b6deab42116d2990896254a00122665c
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-08-30 14:03:38 +0000

    Add files via upload

---
+++ lab5_q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float x1, x2;
+	cout << "we have to find maximum of two numbers\n";
+	cout << "Enter the two numbers ";
+	cin >> x1 >> x2;
+	if (x1 > x2 ) {
+	cout << x1 << " is larger than " << x2 << endl;
+	}
+	else {
+	cout << x2 << " is larger than " << x1 << endl;
+	
+	return 0;
+}
+}
+	
