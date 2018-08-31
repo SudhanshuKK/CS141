6ed27b219fdf391108e09cc955d9ca88a05c1e9c
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-09-01 00:03:35 +0530

    Add files via upload

---
+++ lab5_q25.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+//we have to print all even numbers between 1 to 100;
+	int a=2;
+	while(a<=100){
+	cout << a << endl ;
+	 a=a+2;
+}	
+	return 0;
+} 
---
+++ lab5_q26.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+//we have to print all odd numbers between 1 and 100;
+	int a=1;
+	while (a<=99){
+	cout << a << endl;
+	a=a+2;
+}}
