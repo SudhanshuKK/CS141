f39e313bbe981e65331fb8bc238f3cdb7f4b6f14
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-09-10 12:14:06 +0000

    Update lab5_q22.cpp

--- lab5_q22.cpp
+++ lab5_q22.cpp
@@ -1,8 +1,11 @@
+//libraries
 #include <iostream>
 using namespace std;
 int main(){
+	//initialising variable
 	int a=1;
 	cout << "we have to print the natural number between 1 to n \n";
+	//using the while loop
 	while(a<=100) {
 	cout << a << endl;
 	a++;
