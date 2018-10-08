8e42cca9f16b9d7d7546bc244df1c61274963add
SudhanshuKK <42139699+SudhanshuKK@users.noreply.github.com>
2018-10-08 15:33:06 +0530

    Add files via upload

---
+++ lab5_q6.cpp
@@ -0,0 +1,16 @@
+//leap year or not
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int x;
+cout<<"enter the year : ";
+cin>>x;
+if(x%4==0){
+cout<<"leap year"<<endl;
+}
+else{
+cout<<"not a leap year"<<endl;}
+return 0;
+}
